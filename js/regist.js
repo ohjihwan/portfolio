26a1468d2e97076668b7c5e37893cdf2d9a194a5
오지환 <ovitwisdom@gmail.com>
2025-04-16 16:34:00 +0900

    이메일 인증 체크 진행중

--- js/regist.js
+++ js/regist.js
@@ -4,11 +4,13 @@ function sendit(){
 	const userpw_re = document.getElementById('userpw_re');
 	const name = document.getElementById('name');
 	const hp = document.getElementById('hp')
+	const email = document.getElementById('email')
 
 	const expIdText = /^[A-Za-z0-9]{4,20}$/;
 	const expPwText = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,20}$/;
 	const expNameText = /^[가-힣]+$/
 	const expHpText = /^\d{3}-\d{3,4}-\d{4}$/
+	const expEmailText = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[!@#$%^&*]){4,30}$/;
 
 	if(userid.value === '') {
 		alert('아이디를 입력해주세요');
@@ -45,6 +47,26 @@ function sendit(){
 		hp.focus();
 		return false;
 	}
+
+	if(email.value === ''){
+		alert('이메일형식이 맞지 않습니다')
+		email.focus();
+		return false;
+	}
+
+// 주민등록 번호
+// 생년월일 - 성별 - 시도번호 - 읍면동번호 - 접수순서 - 검증번호
+// xxxxxx  -  x   -   xx    -     xx    -    x     -   x
+
+
+// 001011 - 3068518
+
+// 1. 마지막 번호를 빼놓습니다
+// 2. 2,3,4,5,7,8,9,1,2,3,4,5를 각자리에 곱합니다.
+// 3. 각 자리에 결과를 모두 더합니다
+// 4. 결과에 11로 나누어 나머지 값을 구합니다.
+// 5. 11에서 나머지 값을 뺍니다. (단 검증숫자가 10 은 '0', 11 은 '1'로 간주)
+// 6. 주민등록번호의 검증숫자와 결과가 같으면 유효한 주민등록번호입니다.
 
 	return true;
 }
