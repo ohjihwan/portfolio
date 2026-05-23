4b0ba56e36af4b77eb6c70e1e7cf8f0b44462c1e
ohjihwan <ohjihwan@naver.com>
2026-05-24 04:15:12 +0900

    포폴 재시작2

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 💡 GitHub Pages 배포를 위해 정적 HTML 내보내기 설정 추가
+  // 💡 정적 HTML로 결과물을 뽑아내겠다는 설정
   output: "export",
-  // 이미지 최적화 기능을 GitHub Pages용 정적 컴파일에 맞게 비활성화
   images: {
     unoptimized: true,
   },
